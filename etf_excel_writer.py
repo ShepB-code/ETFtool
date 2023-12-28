074bbeaa3f93bacc969cafc0ded541073f17e55c
Shep <shepardjberry@gmail.com>
2023-12-28 12:22:17 -0600

    Add summary sheet and in-pytho calcuations

--- etf_excel_writer.py
+++ etf_excel_writer.py
@@ -2,30 +2,33 @@ import json
 import openpyxl as xl
 from datetime import datetime
 
+RAW_FACTOR = 2
+TIME_FACTOR = 100
+
 def excel_writer():
     # open workbook
     wb = xl.Workbook()
-    ws = wb.active
-    ws.title = "ETFs"
+    etf_sheet = wb.active
+    etf_sheet.title = "ETFs"
 
-    ws['A1'] = 'Score Rank'
-    ws['B1'] = 'Index'
-    ws['C1'] = 'Provider'
-    ws['D1'] = 'Ticker'
-    ws['E1'] = 'Remaining Cap %'
-    ws['F1'] = 'Remaining Buffer %'
-    ws['G1'] = 'Downside Before Buffer %'
-    ws['H1'] = 'Remaining Outcome Period'
-    ws['I1'] = 'Starting Cap %'
-    ws['J1'] = 'Cap Used %'
-    ws['K1'] = 'Percentage Used %'
-    ws['L1'] = 'Remaining Percentage %'
-    ws['M1'] = 'One Day Potential Return %'
-    ws['N1'] = 'One Month Potential Return %'
-    ws['O1'] = 'One Year Potential Return %'
-    ws['P1'] = 'Raw Value Sum * Mult'
-    ws['Q1'] = 'Raw Value Mult'
-    ws['Q2'] = 2
+    etf_sheet['A1'] = 'Score Rank'
+    etf_sheet['B1'] = 'Index'
+    etf_sheet['C1'] = 'Provider'
+    etf_sheet['D1'] = 'Ticker'
+    etf_sheet['E1'] = 'Remaining Cap %'
+    etf_sheet['F1'] = 'Remaining Buffer %'
+    etf_sheet['G1'] = 'Downside Before Buffer %'
+    etf_sheet['H1'] = 'Remaining Outcome Period'
+    etf_sheet['I1'] = 'Starting Cap %'
+    etf_sheet['J1'] = 'Cap Used %'
+    etf_sheet['K1'] = 'Percentage Used %'
+    etf_sheet['L1'] = 'Remaining Percentage %'
+    etf_sheet['M1'] = 'One Day Potential Return %'
+    etf_sheet['N1'] = 'One Month Potential Return %'
+    etf_sheet['O1'] = 'One Year Potential Return %'
+    etf_sheet['P1'] = 'Raw Value Sum * Mult'
+    etf_sheet['Q1'] = 'Raw Value Mult'
+    etf_sheet['Q2'] = RAW_FACTOR
  
     # get json
     with open('etf_data.json', 'r') as json_file: 
@@ -34,21 +37,21 @@ def excel_writer():
         current_cell = 2
         for provider, data in etf_data.items():
             for etf, values in data.items():
-                ws.cell(current_cell, 2).value = current_cell - 1 # Score Rank
-                ws.cell(current_cell, 3).value = provider # Provider
-                ws.cell(current_cell, 4).value = etf # Ticker
-                ws.cell(current_cell, 5).value = values['remaining_cap'] # Remaining Cap %
-                ws.cell(current_cell, 6).value = values['remaining_buffer'] # Remaining Buffer %
-                ws.cell(current_cell, 7).value = values['downside_before_buffer'] # Downside Before Buffer %
-                ws.cell(current_cell, 8).value = values['remaining_outcome_period'] # Remaining Outcome Period (days)
-                ws.cell(current_cell, 9).value = values['starting_cap'] # Starting Cap %
-                ws.cell(current_cell, 10).value = f'=I{current_cell} - E{current_cell}' # Cap Used %
-                ws.cell(current_cell, 11).value = f'=J{current_cell}/I{current_cell}' # Percentage Used %
-                ws.cell(current_cell, 12).value = f'=1-K{current_cell}' # Remaining Percentage %
-                ws.cell(current_cell, 13).value = f'=E{current_cell}/H{current_cell}' # One day potential return %
-                ws.cell(current_cell, 14).value = f'=M{current_cell}*30.5' # One month Potential Return %
-                ws.cell(current_cell, 15).value = f'=M{current_cell}*365' # One Year Potential Return %
-                ws.cell(current_cell, 16).value = f'=(E{current_cell}+F{current_cell}+G{current_cell})*$Q$2' # Raw Sum * Mult
+                etf_sheet.cell(current_cell, 2).value = current_cell - 1 # Score Rank
+                etf_sheet.cell(current_cell, 3).value = provider # Provider
+                etf_sheet.cell(current_cell, 4).value = etf # Ticker
+                etf_sheet.cell(current_cell, 5).value = values['remaining_cap'] # Remaining Cap %
+                etf_sheet.cell(current_cell, 6).value = values['remaining_buffer'] # Remaining Buffer %
+                etf_sheet.cell(current_cell, 7).value = values['downside_before_buffer'] # Downside Before Buffer %
+                etf_sheet.cell(current_cell, 8).value = values['remaining_outcome_period'] # Remaining Outcome Period (days)
+                etf_sheet.cell(current_cell, 9).value = values['starting_cap'] # Starting Cap %
+                etf_sheet.cell(current_cell, 10).value = f'=I{current_cell} - E{current_cell}' # Cap Used %
+                etf_sheet.cell(current_cell, 11).value = f'=J{current_cell}/I{current_cell}' # Percentage Used %
+                etf_sheet.cell(current_cell, 12).value = f'=1-K{current_cell}' # Remaining Percentage %
+                etf_sheet.cell(current_cell, 13).value = f'=E{current_cell}/H{current_cell}' # One day potential return %
+                etf_sheet.cell(current_cell, 14).value = f'=M{current_cell}*30.5' # One month Potential Return %
+                etf_sheet.cell(current_cell, 15).value = f'=M{current_cell}*365' # One Year Potential Return %
+                etf_sheet.cell(current_cell, 16).value = f'=(E{current_cell}+F{current_cell}+G{current_cell})*$Q$2' # Raw Sum * Mult
 
                 current_cell += 1
 
@@ -61,32 +64,63 @@ def excel_writer():
 
 
     # write to Calculations Sheet
-    ns = wb.create_sheet(title='Calc')
+    
+    calc_sheet = wb.create_sheet(title='Calc')
 
-    ns['A1'] = 'Risk/Reward'
-    ns['B1'] = 'Period/Time Factor'
-    ns['C1'] = 'Rank'
-    ns['D1'] = 'Downside/Buffer'
-    ns['E1'] = 'Score'
+    calc_sheet['A1'] = 'Ticker'
+    calc_sheet['B1'] = 'Risk/Reward'
+    calc_sheet['C1'] = 'Period/Time Factor'
+    calc_sheet['D1'] = 'Rank'
+    calc_sheet['E1'] = 'Downside/Buffer'
+    calc_sheet['F1'] = 'Score'
 
-    ns['F1'] = 'Time Factor'
-    ns['F2'] = 100
+    calc_sheet['G1'] = 'Time Factor'
+    calc_sheet['G2'] = TIME_FACTOR
 
     total_written = current_cell - 2
 
     for i in range(total_written):
-        ns.cell(i + 2, 1).value = f'=ETFs!E{i+2}/ETFs!G{i+2}' # A column
-        ns.cell(i + 2, 2).value = f'=$F$2/ETFs!H{i+2}' # B Column
-        ns.cell(i + 2, 3).value = f'=A{i+2}*B{i+2}' # C Column
-        ns.cell(i + 2, 4).value = f'=ETFs!G{i+2}/ETFs!F{i+2}' # D column
-        ns.cell(i + 2, 5).value = f'=C{i+2}+D{i+2}+ETFs!P{i+2}' # E Column
+        calc_sheet.cell(i + 2, 1).value = f'=ETFs!D{i+2}' # Ticker
+        calc_sheet.cell(i + 2, 2).value = f'=ETFs!E{i+2}/ETFs!G{i+2}' # B column
+        calc_sheet.cell(i + 2, 3).value = f'=$G$2/ETFs!H{i+2}' # C Column
+        calc_sheet.cell(i + 2, 4).value = f'=B{i+2}*C{i+2}' # D Column
+        calc_sheet.cell(i + 2, 5).value = f'=ETFs!G{i+2}/ETFs!F{i+2}' # E column
+        calc_sheet.cell(i + 2, 6).value = f'=D{i+2}+E{i+2}+ETFs!P{i+2}' # F Column
 
 
-    
+    # perform ranking of the ETFS into a summary page
+    with open('etf_data.json', 'r') as json_file:
+        etf_data = json.load(json_file)
+            
+        summary_sheet = wb.create_sheet(title='Summary')
+
+        calc_list = generate_calc_list(etf_data)
+
+
+        # SHOWN BY OVERALL SCORE
+        summary_sheet['A1'] = "Overall Ranking"
+
+        # headers 
+        summary_sheet['A3'] = 'Rank'
+        summary_sheet['B3'] = 'Ticker'
+        summary_sheet['C3'] = 'Score'
+
+        # 1 row of buffer between each
+        start_row = 4
+        calc_list_by_score = sort_calc_list(calc_list)
+        for i in range(10):
+            summary_sheet.cell(start_row + i, 1).value = i + 1
+            summary_sheet.cell(start_row + i, 2).value = calc_list_by_score[i]['ticker'] # ticker
+            summary_sheet.cell(start_row + i, 3).value = calc_list_by_score[i]['score'] # score
+
+
+
     # write score ranking equations in ETF sheet
     for i in range(total_written):
         formula = f'=_xlfn.RANK.EQ(Calc!E{i+2}, Calc!$E$2:Calc!$E${total_written+1})'
-        ws.cell(row=i + 2, column=1, value=formula)
+        etf_sheet.cell(row=i + 2, column=1, value=formula)
+
+
     filename = f'ETFReport_{datetime.now().strftime("%m-%d-%Y")}.xlsx'
 
     # resize all columns in all worksheets and format columns with percentages
@@ -99,6 +133,31 @@ def excel_writer():
 
     return filename
 
+
+
+def generate_calc_list(etf_data):
+    calc_list = []
+
+    for provider, etfs in etf_data.items():
+        for ticker, etf_info in etfs.items():
+            calc_dict = {}
+            calc_dict['ticker'] = ticker
+            calc_dict['raw_sum_mult'] = (etf_info['remaining_cap'] + etf_info['remaining_buffer'] + etf_info['downside_before_buffer']) * RAW_FACTOR
+            calc_dict['risk/reward'] = etf_info['remaining_cap'] / etf_info['downside_before_buffer']
+            calc_dict['period/time_factor'] = TIME_FACTOR / etf_info['remaining_outcome_period']
+            calc_dict['rank'] = calc_dict['risk/reward'] * calc_dict['period/time_factor']
+            
+            calc_dict['downside/buffer'] = etf_info['downside_before_buffer'] / etf_info['remaining_buffer']
+
+            calc_dict['score'] = calc_dict['raw_sum_mult'] + calc_dict['rank'] + calc_dict['downside/buffer']
+
+            calc_list.append(calc_dict)
+
+
+    return calc_list
+
+def sort_calc_list(calc_list, sort_key='score'):
+    return sorted(calc_list, key = lambda calc_dict : calc_dict[sort_key], reverse=True)
 
 def as_text(value):
     if value is None or (isinstance(value, str) and '=' in value):
@@ -115,7 +174,7 @@ def format_percentages(worksheet):
 
     for cell in header_row:
         # if this should be represented as a % row, then format
-        if '%' in cell.value:
+        if cell.value and '%' in cell.value:
             for cell in worksheet[cell.column_letter][1:]:
                 cell.number_format = '0.00%'
 
