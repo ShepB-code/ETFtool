0eece9a8542045e90e0e3ff1181b1ef9ffabcfb3
ShepB-code <shepardjberry@gmail.com>
2024-01-22 21:13:18 -0600

    change port

--- server.py
+++ server.py
@@ -25,6 +25,6 @@ def home():
     return 'Ready to go!'
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6969))
+    port = int(os.environ.get("PORT", 7070))
     app.run(port=port, debug=True, host='0.0.0.0')
 
